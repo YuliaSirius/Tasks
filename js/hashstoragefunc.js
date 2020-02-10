8472a852f8108d707bd0a9007da6b740e8723bee
YuliaSirius <0juli0@tut.by>
2020-02-10 15:05:40 +0300

    10.02.20

--- js/hashstoragefunc.js
+++ js/hashstoragefunc.js
@@ -1,31 +1,61 @@
 'use strict';
 function HashStorageFunc() {
-  let hash = {};
-  this.addvalue = function addValue(key, value) {
-    hash[key] = value;
+  this._hash = {};
+  this.addValue = function(key, value) {
+    this._hash[key] = value;
   };
-  this.getvalue = function getValue(key) {
-    return hash[key];
+  this.getValue = function(key) {
+    return this._hash[key];
   };
-  this.delete = function deleteValue(key) {
-    return hash[key] ? delete hash[key] : false;
+  this.deleteKey = function(key) {
+    return this._hash[key] ? delete this._hash[key] : false;
   };
-  this.getkeys = function getKeys() {
-    return Object.keys(hash);
+  this.getKeys = function() {
+    return Object.keys(this._hash);
+  };
+}
+
+function ClassA() {
+  HashStorageFunc.apply(this, arguments);
+  this.countDrinks = function() {
+    return Object.keys(this._hash).length;
+  };
+  let parentAddValue = this.addValue;
+  this.addValue = function(key) {
+    parentAddValue.apply(this, arguments);
+    alert(`${key} added to list`);
+    return true;
+  };
+}
+
+function ClassB() {
+  HashStorageFunc.apply(this, arguments);
+  this.deleteAll = function() {
+    for (let key in this._hash) {
+      delete this._hash[key];
+    }
+  };
+  let parentGetValue = this.getValue;
+  this.getValue = function(key) {
+    parentGetValue.apply(this, arguments);
+    if (this._hash[key]) {
+      alert(`${key} is on the list`);
+    } else alert("This drink isn't on the list");
   };
 }
 
 let drinkStorage = new HashStorageFunc();
+let classA = new ClassA();
+let classB = new ClassB();
 
-drinkStorage.addvalue('Margarita', {
-  Alcoholic: 'yes',
-  Ingredients: ['Strawberry', 'Tequila']
-});
-drinkStorage.addvalue('Pina Colada', {
-  Alcoholic: 'yes',
-  Ingredients: ['Сoconut milk', 'Pineapple juice']
-});
-console.log(drinkStorage.getvalue('Margarita'));
-console.log(drinkStorage.delete(' '));
-console.log(drinkStorage.getkeys());
-console.log(drinkStorage);
+// drinkStorage.addValue('Margarita', {
+//   Alcoholic: 'yes',
+//   Ingredients: ['Strawberry', 'Tequila']
+// });
+// classB.addValue('Pina Colada', {
+//   Alcoholic: 'yes',
+//   Ingredients: ['Сoconut milk', 'Pineapple juice']
+// });
+// console.log(classB);
+// console.log(classB.getKeys());
+// classB.getValue('Pina Coada');
