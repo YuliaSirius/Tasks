209f8570b0347acb0a4172a5538525cb7d0bca8d
YuliaSirius <0juli0@tut.by>
2020-02-14 01:07:21 +0300

    14.02.2020

---
+++ js/arrFilter.js
@@ -0,0 +1,10 @@
+'use strict';
+let arr = [1, 5, 15, 2.58, 38, 12, 17, 25, 4];
+
+let inRange = (min, max) => item => min <= item && item <= max;
+
+let filtered = arr.filter(inRange(3, 15)); // [5, 15, 12, 4]
+
+
+['1.2', '15'].map(parseInt); // [1, NaN]
+['1.2', '15'].map(item => item = parseInt(item)); // [1, 15]
